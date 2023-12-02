822fd943d9ff6f4b9f46d241b36f50e04a3a1eb2
Nick Leso <nickleso.work@gmail.com>
2023-12-02 21:29:16 +0200

    hero and mobile menu full
    - hero and mobile menu full
    - section presentation witout noise
    - insert inline svgs

--- index.js
+++ index.js
@@ -28,3 +28,31 @@ function onFeedbackFormSubmit(event) {
 
   form.reset();
 }
+
+// burger menu
+const burgerButton = document.getElementById("burger-button");
+const burgerIcon = document.getElementById("burger-icon");
+const burgerMenu = document.getElementById("burger-menu");
+const burgerLink = document.querySelectorAll(".burger-link");
+
+burgerButton.addEventListener("click", onBurgerButtonClick);
+
+function onBurgerButtonClick() {
+  document.body.classList.toggle("mobile-open");
+  burgerIcon.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
+}
+
+burgerLink.forEach((link) => {
+  link.addEventListener("click", onBurgerLinkClick);
+});
+
+function onBurgerLinkClick() {
+  closeMobileMenu();
+}
+
+function closeMobileMenu() {
+  document.body.classList.remove("mobile-open");
+  burgerIcon.classList.remove("active");
+  burgerMenu.classList.remove("active");
+}
